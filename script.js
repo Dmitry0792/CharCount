ac559d8de00e65b7a09c757d7fb4320f72c79a9c
dmitry0792 <89214075008@mail.ru>
2021-11-27 23:01:18 +0300

    right change

---
+++ script.js
@@ -0,0 +1,48 @@
+let inputIn = document.getElementById("input-in");
+let btn1 = document.querySelector('.btn1');
+let btn2 = document.querySelector('.btn2');
+let divIn = document.querySelector('.div_in');
+let divOut = document.querySelector(".win_out");
+
+btn1.onclick = function () {
+  let a = '';
+  divIn.value = a;
+  divOut.innerHTML = a;
+  
+};
+
+btn2.onclick = function () {
+  let b = wordsCounter(inputIn.value);
+  divOut.innerHTML = b;
+};
+document.addEventListener("keydown", function (e) {
+  if (e.keyCode === 13) {
+    let b = wordsCounter(inputIn.value);
+    divOut.innerHTML = b;
+  }
+});
+
+const wordsCounter = (str1) => {
+  let i = 0;
+  const arr = [];
+  let res = "";
+  for (; i < str1.length; i++) {
+    res = findSym(str1, str1[i]);
+
+    if (!arr.includes(res)) {
+      arr.push(res);
+    }
+  }
+
+  return arr;
+};
+
+const findSym = (str, sym) => {
+  let count1 = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === sym) {
+      count1++;
+    }
+  }
+  return sym + ' - ' + count1;
+};
